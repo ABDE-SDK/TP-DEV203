10b7e89bd616b853c1deab4a09c0871a7e6cef84
sidkiabdo <abderrahmane.sidki@gmail.com>
2025-12-02 18:19:45 +0100

    Ajouter Ex pour pratiquer Incremen...

---
+++ TP0-JS/vv.js
@@ -0,0 +1,14 @@
+// let a = 5;
+// const red = () => { return a++ }
+
+// console.log(red());
+// console.log(a)
+
+////////
+/*
+function produit(a, b, c) {
+  return a * b * c;
+}
+let numbers = [2, 3, 4,5 ,44];
+console.log(produit(...numbers)); 
+*/
