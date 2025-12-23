aa29861cf64c2ebd4b54418d4bd94e446a71684c
sidkiabdo <abderrahmane.sidki@gmail.com>
2025-12-23 18:11:58 +0100

    Add password validator example

--- Examples/Ex2-useState/src/App.jsx
+++ Examples/Ex2-useState/src/App.jsx
@@ -1,12 +1,23 @@
+import React from 'react';
+import { useState } from 'react';
 
-import './App.css'
 
-function App() {
-
+export default function Validator() {
+  const [password, setPassword] = useState('')
   return (
-
-    <></>
+    <>
+      <div className='input-group'>
+        <label htmlFor="password">Enter your password: </label>
+        <input
+          id="password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+        />
+      </div>
+      <div>
+        <p className='results'>{password.length
+          < 4 ? "Password should be at least 4 characters" : "strong password"}</p>
+      </div>
+    </>
   )
 }
-
-export default App
